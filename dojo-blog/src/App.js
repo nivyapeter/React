105eeea50e0bb0e2b833721f5d4d7f80f000a6cb
nivyapeter <nivyakpeter@gmail.com>
2021-12-02 15:02:12 +0530

    addying style

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Navbar from './navbar';
 import Home from './home';
 
 function App() {
-  let title = 'Welcome to the blog';
+  // let title = 'Welcome to the blog';
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        {/* <h1>{title}</h1> */}
         <Home />
       </div>
     </div>
--- dojo-blog/src/navbar.js
+++ dojo-blog/src/navbar.js
@@ -4,7 +4,12 @@ const Navbar = () => {
             <h1>The dojo blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New blog</a>
+                <a href="/create" style={{
+                    color:"white",
+                    backgroundColor:"green",
+                    border:"1px solid gray",
+                    borderRadius:"8px"
+                }}>New blog</a>
             </div>
 
         </nav>
