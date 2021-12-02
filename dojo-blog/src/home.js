43164d59c50705b56667e7153464303dbe4567b0
nivyapeter <nivyakpeter@gmail.com>
2021-12-02 17:00:28 +0530

    dev tools

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,7 +1,18 @@
+import {useState} from 'react';
 const Home = () =>{
+//    let name="mario"; 
+ const [name,setName] = useState('mario');  
+ const [age,setAge] = useState(25);
+ const handClick = () => {
+   setName('Anju') ;
+   setAge(20);
+    }
+    
     return (
     <div className="Home">
     <h2>Home page</h2>
+    <p>{name} is {age} years old</p>
+    <button onClick={handClick}>Click</button>
     </div>
     );
 }
