2957e1e84cb183b7be60752736e39c4f592b2388
nivyapeter <nivyakpeter@gmail.com>
2021-12-02 15:43:20 +0530

    click events

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,7 +1,20 @@
 const Home = () =>{
+    const handleClick = (e) => {
+      console.log("hai", e);
+    }
+    
+    const handleClickAgain =(name,e) => {
+        console.log("hai" + " " + name + e.target);
+
+    }
+
+
+    let link = "https://www.google.com";
     return (
-    <div className="Home">
+    <div className="home">
     <h2>Home page</h2>
+    <button onClick={handleClick}><a href ={link}>Click me</a></button>
+    <button onClick={(e) => handleClickAgain('Anju',e)}>Click me Again</button>
     </div>
     );
 }
