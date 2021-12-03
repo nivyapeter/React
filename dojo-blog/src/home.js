3a6820baf59472e42b86b6ce58d49748dfea5ad0
nivyapeter <nivyakpeter@gmail.com>
2021-12-03 17:16:55 +0530

    output lists

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const Home = () =>{
+    const [blogs,setBlogs] = useState([
+        {title:'My new website', body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.',author:'mario', id:1},
+        {title:'My new party', body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.',author:'mario', id:2},
+        {title:'Web dep top', body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.',author:'mario', id:3}
+    ]);
+    
     return (
     <div className="Home">
-    <h2>Home page</h2>
+    {/* <h2>Home page</h2> */}
+    {blogs.map ((blog) => (
+        <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.author}</p>
+            <p>{blog.body}</p>
+
+        </div>
+    ))}
+    
     </div>
     );
 }
