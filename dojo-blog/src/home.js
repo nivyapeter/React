de6349dca2022b92931f5ead56519f0cd6446f4f
nivyapeter <nivyakpeter@gmail.com>
2021-12-04 16:18:45 +0530

    props and reusable components

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,8 +1,33 @@
-const Home = () =>{
-    return (
+import { useState } from "react";
+import BlogList from "./bloglists";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
+      author: "neetha",
+      id: 1,
+    },
+    {
+      title: "My new party",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
+      author: "mario",
+      id: 2,
+    },
+    {
+      title: "Web dep top",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
+  return (
     <div className="Home">
-    <h2>Home page</h2>
+      <BlogList blogs={blogs} title={"hai all!"} />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"mario's blogs"} />
     </div>
-    );
-}
-export default Home
+  );
+};
+export default Home;
