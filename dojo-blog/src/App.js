fbb42f0dba5e0eda81ea1848810443e902fd2a92
nivyapeter <nivyakpeter@gmail.com>
2021-12-04 15:18:03 +0530

    useState and useEffect

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,17 +1,20 @@
 // import logo from './logo.svg';
-import './App.css';
-import Navbar from './navbar';
-import Home from './home';
+// import './App.css';
+// import Navbar from './navbar';
+// import Home from './home';
+import Ussage from './ussage';
 
 function App() {
   let title = 'Welcome to the blog';
   return (
     <div className="App">
-      <Navbar />
-      <div className="content">
+      {/* <Navbar /> */}
+      <div className="content"> 
         <h1>{title}</h1>
-        <Home />
+        {/* <Home /> */}
+        <Ussage />
       </div>
+      
     </div>
   );
 }
