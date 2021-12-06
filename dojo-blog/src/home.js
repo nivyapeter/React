e04d4c331591328b77fb14fea6c388d9e8bb48d2
nivyapeter <nivyakpeter@gmail.com>
2021-12-06 15:38:11 +0530

    userEffect

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./bloglists";
 
 const Home = () => {
@@ -22,15 +22,22 @@ const Home = () => {
       id: 3,
     },
   ]);
+  const [name,setName] = useState('mario');
+
  const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
  }
+ useEffect(() => {
+   console.log('use effect ran')
+ }, [name] );
   return (
     <div className="Home">
       <BlogList blogs={blogs} title={"hai all!"} handleDelete={handleDelete} />
       <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"mario's blogs"} />
       <BlogList blogs={blogs} handleDelete={handleDelete} />
+      <button onClick = {() => setName('ligi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
