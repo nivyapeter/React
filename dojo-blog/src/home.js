7bae2f62c71e8b871fea390588654e745bad2297
nivyapeter <nivyakpeter@gmail.com>
2021-12-07 12:35:54 +0530

    Fetching data from json server

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -2,26 +2,26 @@ import { useEffect, useState } from "react";
 import BlogList from "./bloglists";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My new website",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
-      author: "neetha",
-      id: 1,
-    },
-    {
-      title: "My new party",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
-      author: "mario",
-      id: 2,
-    },
-    {
-      title: "Web dep top",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null
+    // {
+    //   title: "My new website",
+    //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
+    //   author: "neetha",
+    //   id: 1,
+    // },
+    // {
+    //   title: "My new party",
+    //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
+    //   author: "mario",
+    //   id: 2,
+    // },
+    // {
+    //   title: "Web dep top",
+    //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
+    //   author: "mario",
+    //   id: 3,
+    // },
+  );
   const [name,setName] = useState('mario');
 
  const handleDelete = (id) => {
@@ -29,15 +29,23 @@ const Home = () => {
     setBlogs(newBlogs);
  }
  useEffect(() => {
-   console.log('use effect ran')
- }, [name] );
+   fetch('http://localhost:8000/blogs')
+   .then(res => {
+     return res.json();
+   })
+   .then(data => {
+     console.log(data);
+     setBlogs(data)
+   });
+ }, [] );
   return (
     <div className="Home">
-      <BlogList blogs={blogs} title={"hai all!"} handleDelete={handleDelete} />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"mario's blogs"} />
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
-      <button onClick = {() => setName('ligi')}>change name</button>
+      
+     {blogs && <BlogList blogs={blogs} title={"hai all!"} handleDelete={handleDelete} />}
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"mario's blogs"} />
+      <BlogList blogs={blogs} handleDelete={handleDelete} /> */}
       <p>{name}</p>
+      <button onClick = {() => setName('ligi')}>change name</button>
     </div>
   );
 };
