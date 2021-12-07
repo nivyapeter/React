2fed8ca0bea33697f6b5fc671c5dd1ca27b1a152
nivyapeter <nivyakpeter@gmail.com>
2021-12-07 16:17:27 +0530

    custom hooks

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import BlogList from "./bloglists";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null
+  // const [blogs, setBlogs] = useState(null
     // {
     //   title: "My new website",
     //   body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, odit.",
@@ -21,45 +22,47 @@ const Home = () => {
     //   author: "mario",
     //   id: 3,
     // },
-  );
-  const[isPending,setPending] = useState(true);
-  const [error,setError] = useState(null);
-  const [name,setName] = useState('mario');
+  // );
+  // const[isPending,setPending] = useState(true);
+  // const [error,setError] = useState(null);
+  // const [name,setName] = useState('mario');
 
- const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
- }
- useEffect(() => {
-   setTimeout(() => {
-    fetch('http://localhost:8001/blogss')
-      .then(res => {
-        if(!res.ok){
-          throw Error('could not fetch the data for that responce')
-        }
-        return res.json();
-      })
-      .then(data => {
-        console.log(data);
-        setBlogs(data);
-        setPending(false);
-      })
-      .catch((err => {
-        // console.log(err.message);
-        setError(err.message);
-        setPending(false);
-      }))
-   },1000);
- }, [] );
-  return (
+//  const handleDelete = (id) => {
+//     const newBlogs = blogs.filter(blog => blog.id !== id);
+//     setBlogs(newBlogs);
+//  }
+//  useEffect(() => {
+//    setTimeout(() => {
+//     fetch('http://localhost:8001/blogss')
+//       .then(res => {
+//         if(!res.ok){
+//           throw Error('could not fetch the data for that responce')
+//         }
+//         return res.json();
+//       })
+//       .then(data => {
+//         console.log(data);
+//         setBlogs(data);
+//         setPending(false);
+//       })
+//       .catch((err => {
+//         // console.log(err.message);
+//         setError(err.message);
+//         setPending(false);
+//       }))
+//    },1000);
+//  }, [] );
+  const {data:blogs,isPending,error}= useFetch('http://localhost:8001/blogs');
+
+return (
     <div className="Home">
       {error && <div>{ error }</div>}
       {isPending && <div>Loading...</div>}
-     {blogs && <BlogList blogs={blogs} title={"hai all!"} handleDelete={handleDelete} />}
+     {blogs && <BlogList blogs={blogs} title={"hai all!"} />}
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"mario's blogs"} />
       <BlogList blogs={blogs} handleDelete={handleDelete} /> */}
-      <p>{name}</p>
-      <button onClick = {() => setName('ligi')}>change name</button>
+      {/* <p>{name}</p>
+      <button onClick = {() => setName('ligi')}>change name</button> */}
     </div>
   );
 };
---
+++ dojo-blog/src/useFetch.js
@@ -0,0 +1,32 @@
+// import React from "react";
+import { useEffect, useState } from "react";
+
+const useFetch=(url) => {
+    const [data, setData] = useState(null);
+    const[isPending,setPending] = useState(true);
+    const [error,setError] = useState(null);
+    
+    useEffect(() => {
+            setTimeout(() => {
+            fetch(url)
+            .then(res => {
+                if(!res.ok){
+                throw Error('could not fetch the data for that responce')
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setData(data);
+                setPending(false);
+            })
+            .catch((err => {
+                setError(err.message);
+                setPending(false);
+            }))
+            },1000);
+        }, [url] );
+    return {data,isPending,error}
+}
+
+export default useFetch
