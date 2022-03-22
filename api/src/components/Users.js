c9c9f1116c958b62be5b42f396a2031230280645
nivyapeter <nivyakpeter@gmail.com>
2022-03-23 00:24:39 +0530

    API calling

--- api/src/components/Users.js
+++ api/src/components/Users.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./users.css";
 import axios from "axios";
 
 export default function Users() {
+    const [data,setData] = useState();
+
+const fetchUsers =async () => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    console.log(response);
+    setData(response.data);
+}
+
   return <div className="users">Users
   <div className=""> 
-    <button>Get Users</button>
+    <button onClick={fetchUsers}>Get Users</button>
+    {data ?.map((user) => {
+        return(
+        <div key={user.id}>
+            {user.name}
+            
+        </div>
+        );
+    })}
   </div>
   </div>;
 }
