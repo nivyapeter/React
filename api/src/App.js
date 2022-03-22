ad0ac314aafae7755c68467a482d818da97f5816
nivyapeter <nivyakpeter@gmail.com>
2022-03-22 23:33:37 +0530

    API calling

---
+++ api/src/App.js
@@ -0,0 +1,11 @@
+import './app.css';
+import Users from './components/Users';
+function App() {
+  return (
+    <div className="App">
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ api/src/components/Users.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./users.css";
+import axios from "axios";
+
+export default function Users() {
+  return <div className="users">Users
+  <div className=""> 
+    <button>Get Users</button>
+  </div>
+  </div>;
+}
